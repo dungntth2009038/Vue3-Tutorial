a49792efa41c1206e5173556a3462f6882bb792d
Dung <dungnt@2nf.com.vn>
2022-08-08 17:30:25 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+<h1>Hello app</h1>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to='.modals' v-if="showModal">
+    <ModalUser theme = 'sale' @close='toggleModal'><!-- sử dụng Emitting custom events --> 
+    <template v-slot:links>
+      <a href="#">sign up now</a>
+      <a href="#">more info</a>
+    </template>
+    <h1>Ninja Giveaway</h1>
+    <p>Grab you ninja swag for half price!</p>
+    </ModalUser>
+  </teleport>
+
+<teleport to='.modals' v-if="showModalTwo">
+    <ModalUser @close='toggleModalTwo'><!-- sử dụng Emitting custom events --> 
+    <h1>Sign up to the newsletter</h1>
+    <p>For updates and promo codes</p>
+    </ModalUser>
+</teleport>
+
+  <button @click.alt="toggleModal">Open Modal(alt)</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
+  <p></p>
+  <button @click="count++">add 1</button>
+  <p>Count is: {{count}}</p>
+  <UserList msg ="Chuyển data từ cha sang con!"/>
+  
+</template> 
+
+<script>
+import ModalUser from "./components/ModalUser.vue";
+import UserList from "./components/UserList.vue"
+
+export default {
+  name: "App",
+  components: { ModalUser , UserList },
+  data() {
+    return {
+      title: 'My First Vue App :))',
+      count : 0,
+      showModal: false, // sử dụng Emitting custom events
+      showModalTwo: false,
+    };
+  },
+ methods: {
+  toggleModal(){
+    this.showModal = !this.showModal // sử dụng Emitting custom events
+  },
+  toggleModalTwo(){
+    this.showModalTwo = !this.showModalTwo // sử dụng Emitting custom events
+  },
+ }
+};
+</script>
+
+<style>
+#app, .models{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/ModalUser.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <!--  sử dụng Emitting custom events -->
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!--Sử dụng kiểu boolean-->
+      <slot>defaul content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  // sử dụng Emitting custom events
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 8px;
+  border: 1px solid #eee;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions {
+  color: white;
+}
+.modal.sale .actions a {
+  color: white;
+}
+</style>
